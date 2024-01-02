ad06fefb79458d3856e8cebbd1c42450e55192c0
ablecp <tasong12@kookmin.ac.kr>
2024-01-02 19:24:44 +0900

    comment:장바구니 관련 라우터 swagger 작성

--- routes/customerRoute.js
+++ routes/customerRoute.js
@@ -2,15 +2,104 @@ const express = require('express');
 const { getCart, postCart, updateCart, deleteCart } = require("../controllers/cart_wish");
 const customerRouter = express.Router();
 
+/**
+ * @swagger
+ *  /customer/cart/{userId}:
+ *    get:
+ *      tags:
+ *      - 구매자
+ *      description: 구매자 장바구니 정보를 검색함
+ *      produces:
+ *      - application/json
+ *      parameters:
+ *        - in: path
+ *          name: userId
+ *          required: true
+ *          description: 유저 아이디
+ *          schema:
+ *            type: string
+ *      responses:
+ *       200:
+ *        description: 유저 정보 검색 성공
+ */
 customerRouter.get('/cart/:id', getCart);
 // customerRouter.get('/wish/:id');
-
+/**
+ * @swagger
+ *  /user/join:
+ *     post:
+ *       tags:
+ *         - 구매자
+ *       description: 장바구니를 등록함
+ *       produces:
+ *         - application/json
+ *       requestBody:
+ *         description: 상품 정보를 입력
+ *         required: true
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 customer_id:
+ *                   type: string
+ *                 item_id:
+ *                   type: string
+ *                 amount:
+ *                   type: number
+ *               required:
+ *                 - customer_id
+ *                 - item_id
+ *                 - amount
+ *       responses:
+ *         201:
+ *           description: 상품 등록 성공
+ */
 customerRouter.post('/cart/post', postCart);
 // customerRouter.post('/wish/post',);
-
+/**
+ * @swagger
+ *  /customer/user/{item_id}:
+ *     patch:
+ *       tags:
+ *         - 구매자
+ *       description: 장바구니 수량 수정
+ *       produces:
+ *         - application/json
+ *       requestBody:
+ *         description: 장바구니 아이디와 json형태의 수정 정보 입력
+ *         required: true
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 amount:
+ *                   type: number
+ *       responses:
+ *         200:
+ *           description: 유저 수정 성공
+ */
 customerRouter.patch('/cart/patch/:id', updateCart);
 // customerRouter.patch('/wish/patch/:id');
-
+/**
+ * @swagger
+ *  /customer/cart/delete/{orderId}}:
+ *     delete:
+ *       tags:
+ *         - 구매자
+ *       description: 장바구니를 삭제함
+ *       parameters:
+ *        - in: path
+ *          name: id
+ *          required: true
+ *          schema:
+ *            type: UUID
+ *            description: 장바구니 고유 아이디
+ *       responses:
+ *         201:
+ *           description: 상품 삭제 성공
+ */
 customerRouter.delete('/cart/delete/:id', deleteCart);
 // customerRouter.delete('/wish/delete/:id');
 
--- routes/mainroute.js
+++ routes/mainroute.js
@@ -5,8 +5,11 @@ const customerRouter = require('./customerRoute.js');
 
 const router = express.Router();
 
+//상품관련 라우터
 router.use('/category', categoryRouter);
+//계정관련 라우터
 router.use('/user', userRouter);
+//구매자관련 - 장바구니, 찜, 주문
 router.use('/customer', customerRouter);
 
 //Not Found
